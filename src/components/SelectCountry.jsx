59a796426eb889276598fee01ad05ebf11b1fe16
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-06-02 12:19:49 +0530

    Update SelectCountry.jsx

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -5,6 +5,15 @@ const SelectCountry=(pro)=> {
   const {value, setValue, label}=pro;
   const [data, loaded, error] = useAxios("https://restcountries.com/v3.1/all");
   
+  if(loaded){
+    return(
+      <Grid item xs={12} md={3}>
+        <Skeleton variant="rounded" height={60}/>
+      </Grid>
+    )
+  }
+
+ 
   return (
     
   )
