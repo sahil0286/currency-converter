b418cdd71525088c99ba9b493665d93fc61ec41d
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-06-02 12:17:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { Button } from '@mui/material'
+import { Box, Container, Grid, Typography } from '@mui/material'
 import './App.css'
+import InputAmount from './components/InputAmount'
+import SelectCountry from './components/SelectCountry'
+import SwitchCurrency from './components/SwitchCurrency'
+import { useContext, useEffect, useState } from 'react'
+import { CurrencyContext } from './context/CurrencyContext'
+import axios from 'axios'
 
 function App() {
+  const {
+    fromCurrency, setFromCurrency, toCurrency, setToCurrency, firstAmount , setFirstAmount 
+  } = useContext(CurrencyContext)
+  const [resultCurrency, setResultCurrency] = useState(0)
+  const codeFromCurrency = fromCurrency.split(" ") [0];
+  const codeToCurrency = toCurrency.split(" ") [0];
+
+  
+
+  
   return (
-    <>
-    <Button>
-      Click
-    </Button>
-    </>
+    
   )
 }
 
